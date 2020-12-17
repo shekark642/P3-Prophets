3f77521f777311254311ce3a8463c24c1e9a1a43
OpticStranger <irfannasim61@gmail.com>
2020-12-17 13:00:52 -0800

    Committed the images for our pygame for blackjack

---
+++ blackjack.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+#Import Library
+import pygame
+from pygame.locals import *
+import random
+import copy
+
+#Load Images
+icon = pygame.image.load('resources/icon.png')
+cBack = pygame.image.load('resources/cards/cardback.png')
+diamondA = pygame.image.load('resources/cards/ad.png')
+clubA = pygame.image.load('resources/cards/ac.png')
+heartA = pygame.image.load('resources/cards/ah.png')
+spadeA = pygame.image.load('resources/cards/as.png')
+diamond2 = pygame.image.load('resources/cards/2d.png')
+club2 = pygame.image.load('resources/cards/2c.png')
+heart2 = pygame.image.load('resources/cards/2h.png')
+spade2 = pygame.image.load('resources/cards/2s.png')
+diamond3 = pygame.image.load('resources/cards/3d.png')
+club3 = pygame.image.load('resources/cards/3c.png')
+heart3 = pygame.image.load('resources/cards/3h.png')
+spade3 = pygame.image.load('resources/cards/3s.png')
+diamond4 = pygame.image.load('resources/cards/4d.png')
+club4 = pygame.image.load('resources/cards/4c.png')
+heart4 = pygame.image.load('resources/cards/4h.png')
+spade4 = pygame.image.load('resources/cards/4s.png')
+diamond5 = pygame.image.load('resources/cards/5d.png')
+club5 = pygame.image.load('resources/cards/5c.png')
+heart5 = pygame.image.load('resources/cards/5h.png')
+spade5 = pygame.image.load('resources/cards/5s.png')
+diamond6 = pygame.image.load('resources/cards/6d.png')
+club6 = pygame.image.load('resources/cards/6c.png')
+heart6 = pygame.image.load('resources/cards/6h.png')
+spade6 = pygame.image.load('resources/cards/6s.png')
+diamond7 = pygame.image.load('resources/cards/7d.png')
+club7 = pygame.image.load('resources/cards/7c.png')
+heart7 = pygame.image.load('resources/cards/7h.png')
+spade7 = pygame.image.load('resources/cards/7s.png')
+diamond8 = pygame.image.load('resources/cards/8d.png')
+club8 = pygame.image.load('resources/cards/8c.png')
+heart8 = pygame.image.load('resources/cards/8h.png')
+spade8 = pygame.image.load('resources/cards/8s.png')
+diamond9 = pygame.image.load('resources/cards/9d.png')
+club9 = pygame.image.load('resources/cards/9c.png')
+heart9 = pygame.image.load('resources/cards/9h.png')
+spade9 = pygame.image.load('resources/cards/9s.png')
+diamond10 = pygame.image.load('resources/cards/10d.png')
+club10 = pygame.image.load('resources/cards/10c.png')
+heart10 = pygame.image.load('resources/cards/10h.png')
+spade10 = pygame.image.load('resources/cards/10s.png')
+diamondJ = pygame.image.load('resources/cards/jd.png')
+clubJ = pygame.image.load('resources/cards/jc.png')
+heartJ = pygame.image.load('resources/cards/jh.png')
+spadeJ = pygame.image.load('resources/cards/js.png')
+diamondQ = pygame.image.load('resources/cards/qd.png')
+clubQ = pygame.image.load('resources/cards/qc.png')
+heartQ = pygame.image.load('resources/cards/qh.png')
+spadeQ = pygame.image.load('resources/cards/qs.png')
+diamondK = pygame.image.load('resources/cards/kd.png')
+clubK = pygame.image.load('resources/cards/kc.png')
+heartK = pygame.image.load('resources/cards/kh.png')
+spadeK = pygame.image.load('resources/cards/ks.png')
+
+#Set Icon
+pygame.display.set_icon(icon)
+
+#Global Constants
+black = (0,0,0)
+white = (255,255,255)
+gray = (192,192,192)
+
+cards = [ diamondA, clubA, heartA, spadeA, \
+          diamond2, club2, heart2, spade2, \
+          diamond3, club3, heart3, spade3, \
+          diamond4, club4, heart4, spade4, \
+          diamond5, club5, heart5, spade5, \
+          diamond6, club6, heart6, spade6, \
+          diamond7, club7, heart7, spade7, \
+          diamond8, club8, heart8, spade8, \
+          diamond9, club9, heart9, spade9, \
+          diamond10, club10, heart10, spade10, \
+          diamondJ, clubJ, heartJ, spadeJ, \
+          diamondQ, clubQ, heartQ, spadeQ, \
+          diamondK, clubK, heartK, spadeK ]
+cardA = [ diamondA, clubA, heartA, spadeA ]
+card2 = [ diamond2, club2, heart2, spade2 ]
+card3 = [ diamond3, club3, heart3, spade3 ]
+card4 = [ diamond4, club4, heart4, spade4 ]
+card5 = [ diamond5, club5, heart5, spade5 ]
+card6 = [ diamond6, club6, heart6, spade6 ]
+card7 = [ diamond7, club7, heart7, spade7 ]
+card8 = [ diamond8, club8, heart8, spade8 ]
+card9 = [ diamond9, club9, heart9, spade9 ]
+card10 = [ diamond10, club10, heart10, spade10, \
+           diamondJ, clubJ, heartJ, spadeJ, \
+           diamondQ, clubQ, heartQ, spadeQ, \
+           diamondK, clubK, heartK, spadeK ]
