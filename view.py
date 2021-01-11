0fd55e213b9ddf99eb57b55453b4592048be6789
MaxVukovich <maxvukov@gmail.com>
2021-01-11 13:06:47 -0800

    added blackjack file and temp menu

--- view.py
+++ view.py
@@ -14,9 +14,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/leaderboards/')
+@app.route('/leaderboards')
 def leaderboards():
     return render_template("leaderboards.html")
+
+@app.route('/blackjack')
+def blackjack():
+    return render_template("blackjack.html")
 
 if __name__ == "__main__":
     # runs the application on the repl development server
