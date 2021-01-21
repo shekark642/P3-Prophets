25dbb1881498f4f699fbce424447c1e8354cc2b9
OpticStranger <72882180+OpticStranger@users.noreply.github.com>
2021-01-20 22:35:18 -0800

    Created Login Database for Website

--- view.py
+++ view.py
@@ -1,7 +1,48 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify, url_for, __init__
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import requests as r
+import json as j
 
-# create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I<+g/P2N$}0GXO00'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# IMPORTANT - GENERATES CSRF TOKEN
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model): #Creates columns inside of the database
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True) #username column
+    email = db.Column(db.String(50), unique=True) #email column
+    password = db.Column(db.String(80)) #password column
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
 
 # connects default URL of server to a python function
 @app.route('/')
@@ -16,6 +57,46 @@ def leaderboards():
 def blackjack():
     return render_template('blackjack.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return render_template('invalid.html', form=form)
+    #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        # redirect to page when user is created
+        return render_template('usercreatedredirect.html')
+    # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/war')
 def war():
@@ -23,6 +104,5 @@ def war():
 
 
 if __name__ == "__main__":
-    # runs the application on the repl development server
     app.run(debug=True)
 
