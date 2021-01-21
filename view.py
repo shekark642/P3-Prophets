439f363e0da6c30b4b6030437a65648f1c82f3e9
MaxVukovich <maxvukov@gmail.com>
2021-01-20 23:37:32 -0800

    newest version i think

--- view.py
+++ view.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from flask_wtf.csrf import CSRFProtect
-from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import requests as r
+import json as j
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I<+g/P2N$}0GXO00'
@@ -102,3 +105,5 @@ def war():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#yolo
