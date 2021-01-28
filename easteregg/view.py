2d125d6fa8b6048c63990aaf002146fabb88203a
OpticStranger <72882180+OpticStranger@users.noreply.github.com>
2021-01-28 12:42:57 -0800

    Blueprint for Easteregg

---
+++ easteregg/view.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for, request
+from easteregg import easteregg_bp
+
+app.register_blueprint(easteregg_bp, url_prefix='/easteregg')
+
+@easteregg_bp.route('/easteregg')
+def easteregg():
+    return render_template("easteregg.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
